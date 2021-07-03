65db297eaf7559c678f7f413529794a2ed31fd1d
Mamun Ahmed Fahim <83030337+noobboss1@users.noreply.github.com>
2021-07-03 20:52:04 +0600

    Create Wish.sh

---
+++ Wish.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+echo "Happy Birthday Shuvo"|lolcat -a -s 99 -d 3
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat
+
+figlet "Birthday"|lolcat
+
+figlet "Shuvo"|lolcat
+
+sleep 1
+
+clear
+
+figlet "Happy"|lolcat|pv -qL 999
+
+echo "I feel so lucky to have you as my friend. Hope your birthday is as special as you are.May all of your dreams come true. Thanks for being such a great friend. Happy birthday. In good times and bad, I’ll always be by your side. Happy birthday friend! Happy, happy birthday! You deserve all the cakes, love, hugs and happiness today. Enjoy your day my friend! May God shower you with blessings today and always. Happy birthday wishes for my friend! Happy, healthy, exceptional, rocking birthday to you my friend! Every step of the way you were there for me. Through thick and thin I’ll always be there for you. Happy birthday wishes! Thanks for another year of being an awesome friend. Happy birthday dear friend!       Love You So Much My Friend!!!"|lolcat|pv -qL 100
+
